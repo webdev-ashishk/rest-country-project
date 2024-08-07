eeef1ad75e89cbade4ae549cd2a3ab0df79e3b44
webdev-ashishk <webdev.ashishk.com>
2024-08-07 11:23:09 +0530

    fixing drop-down error

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -15,12 +15,22 @@ function _filterCountryData(
   return filterCountryData;
 }
 
+function _filterSelectedCountryRegion(
+  selectedRegion: string,
+  allCountry: CountryType[]
+) {
+  const selectedCountryData = allCountry.filter((country: CountryType) =>
+    country?.region?.toLowerCase()?.includes(selectedRegion.toLowerCase())
+  );
+  return selectedCountryData;
+}
 const Body = () => {
   // const [allCountry, setAllCountry] = useState(null);
   const [allCountry, setAllCountry] = useState<CountryType[]>([]);
   const [filterCountry, setFilterCountry] = useState<CountryType[]>([]);
   const [searchText, setSearchText] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [selectedRegion, setSelectedRegion] = useState<string>("");
 
   useEffect(() => {
     fetchAllCountry();
@@ -35,8 +45,8 @@ const Body = () => {
       setErrorMessage("no country found... " + JSON.stringify(error));
     }
   };
-  console.log(allCountry);
-  console.log(filterCountry);
+  // console.log(allCountry);
+  console.log(JSON.stringify(filterCountry));
   const searchCountryData = (
     searchText: string | number,
     allCountry: CountryType[]
@@ -53,6 +63,25 @@ const Body = () => {
       }
     }
   };
+
+  const handleRegionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedRegion(value);
+    searchCountryBasedOnResion(selectedRegion, allCountry);
+  };
+
+  console.log(selectedRegion);
+  const searchCountryBasedOnResion = (
+    selectedRegion: string,
+    allCountry: CountryType[]
+  ) => {
+    const data: CountryType[] = _filterSelectedCountryRegion(
+      selectedRegion,
+      allCountry
+    );
+    setFilterCountry(data);
+  };
+
   // if (!allCountry) return <div>loading...</div>;
   if (allCountry.length === 0) return <div>loading...</div>;
   {
@@ -76,9 +105,9 @@ const Body = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"
                 />
               </svg>
@@ -128,12 +157,21 @@ const Body = () => {
           </button>
         </div>
 
-        {/* drop down */}
-        <select className="bg-gray-700 p-2 m-2">
-          <option value="1">love</option>
-          <option value="2">wow</option>
-          <option value="3">dumm</option>
-          <option value="4">genius</option>
+        {/* DROP-DOWN down */}
+        <label htmlFor="region"> Filter By Region </label>
+
+        <select
+          className="bg-gray-700 p-2 mx-2 my-auto w-[200px]"
+          id="region"
+          name="dropdown"
+          onChange={handleRegionChange}
+          value={selectedRegion}
+        >
+          {allCountry.map((country: CountryType) => (
+            <option value={country.region} key={country.ccn3}>
+              {country.region}
+            </option>
+          ))}
         </select>
       </div>
       <div className="body-last flex flex-wrap justify-evenly">
@@ -155,7 +193,7 @@ const Body = () => {
           };
           return (
             <Link to={`/name/${countryData.name}`}>
-              <ListOfAllCountry key={countryData.ccn3} {...countryData} />
+              <ListOfAllCountry key={countryData.key} {...countryData} />
             </Link>
           );
         })}
